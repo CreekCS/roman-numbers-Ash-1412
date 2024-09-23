27bc4a718749dd3a0ef7e959f8993ee0419edb02
Aasha Shakya <178453028+Ash-1412@users.noreply.github.com>
2024-09-23 19:11:19 +0000

    commit

--- Romans.java
+++ Romans.java
@@ -6,18 +6,22 @@ import java.util.*;
 
 public class Romans
 {
-	private Map<String, Integer> m;
+	private Map<String, Integer> m = new TreeMap<String, Integer>();;
 	
 	public Romans()
-	{
-		m = new TreeMap<String, Integer>();
-		
-		//complete your map				
+	{	
+		m.put("I", 1);	
+		m.put("V", 5);
+		m.put("X", 10);
+		m.put("L", 50);
+		m.put("C", 100);
+		m.put("D", 500);
+		m.put("M", 1000);
 	}
 	
 	public int getNumber( String s )
 	{
-		//complete this method
+		return m.get(s);
 	}
 
 }
